217386368d77c026e0567d5334c8e16220c78522
millociarrocchi <deliveryeduardo21@gmail.com>
2024-02-23 21:58:59 -0500

    creada estructura en html

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
-const port = 3000
-
-app.listen(port, () => 
-console.log("servidor corriendo en puerto " + port))
-
 app.use(express.static(publicPath));
+
+app.listen(3060, () => {
+console.log("servidor corriendo en puerto 3060");
+});
 
 app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/carritoCompras", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/carritoCompras.html"))
 })
 
