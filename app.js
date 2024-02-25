a383ea8a2100a268f1ebeb23cfa58502374dfd4e
LauMilena <lauramile05@gmail.com>
2024-02-25 18:37:30 -0500

    resolucion de conflictos

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
 
 
+
+
+
+
+
+
+
 app.listen(3030, () => 
 console.log("servidor corriendo en puerto "))
 
@@ -17,19 +24,5 @@ app.get("/detalleProducto", function(req, res){
     res.sendFile(path.resolve(__dirname, "./views/detalleProducto.html"))
 })
 
-/*app.get("/register", function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/register.html"))
-})
 
-app.post("/register", function(req, res){
-    res.send("Se registró con exito")
-})
-
-app.get("/login", function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/login.html"))
-})
-
-app.post("/login", function(req, res){
-    res.send("Bienvenido")
-})*/
 
