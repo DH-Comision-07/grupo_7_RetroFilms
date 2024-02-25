970e68d651ee91a51fcf67cc36b510f0838aed64
LauMilena <lauramile05@gmail.com>
2024-02-24 22:42:36 -0500

    creacion site DetalleProducto

--- app.js
+++ app.js
@@ -2,14 +2,34 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
-const port = 3000
 
-app.listen(port, () => 
-console.log("servidor corriendo en puerto " + port))
+
+app.listen(3030, () => 
+console.log("servidor corriendo en puerto "))
 
 app.use(express.static(publicPath));
 
 app.get("/", function(req, res){
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
 })
+
+app.get("/detalleProducto", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/detalleProducto.html"))
+})
+
+/*app.get("/register", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.post("/register", function(req, res){
+    res.send("Se registró con exito")
+})
+
+app.get("/login", function(req, res){
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
+app.post("/login", function(req, res){
+    res.send("Bienvenido")
+})*/
 
