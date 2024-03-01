785f3f17818b7a8fb908d4939810076ffc17222c
LauMilena <lauramile05@gmail.com>
2024-02-29 21:20:39 -0500

    fix: arreglo de emergencia de imagenes

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
 
-app.listen(3030, () => 
+app.listen(3031, () => 
 console.log("servidor corriendo en puerto "));
 
 
 app.use(express.static(publicPath));
 
-app.listen(3030, () => {
-console.log("servidor corriendo en puerto 3060");
-});
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/index.html'))
