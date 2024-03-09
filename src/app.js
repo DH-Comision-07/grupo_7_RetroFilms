c3c3265cba016817894e430aa060aed42757a556
VincentVega20 <maximolopardo@gmail.com>
2024-03-09 13:54:49 -0300

    adicion archivos controllers/models/routes

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const app = express();
 const path = require("path")
 const publicPath = path.resolve(__dirname, "./public")
 const port = 3000
+const indexRouter = require("./routes/index.routes")
+
 
 app.listen(3000, () => 
 console.log('https://localhost:'+ port));
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "/views"));
 
 app.use(express.static(publicPath));
+
+/* app.use('/', indexRouter); */
 
 
 app.get('/', (req, res) =>{
