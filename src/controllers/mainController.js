5d66e37021acc438e18b52f58fb5e4f7eccfe4a9
VincentVega20 <maximolopardo@gmail.com>
2024-03-25 23:02:37 -0300

    cambios generales

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,7 @@
 const productoService = require('../models/productoService.js')
 
 const mainController = { 
-    index: (req, res) => res.render("index", {productos: productoService.getAll()}),
+    index: (req, res) => res.render("index", {productos : productoService.getAll()}),
 }
 
 module.exports = mainController;
