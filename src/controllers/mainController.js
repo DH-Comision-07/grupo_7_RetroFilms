7b2376358f0afa6a2320e061b087a0a6e35399e3
Laura <laura@iMac-de-Laura.local>
2024-03-28 16:45:14 -0500

    mainController

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,7 @@
 const productoService = require('../models/productoService.js')
 
 const mainController = { 
-    index: (req, res) => res.render("index", {carrousellMovies: productoService.getCarrousell(),movieGridMovies: productoService.getMovieGrid(), topMovies: productoService.getTopMovies()}),
+    index: (req, res) => res.render("index", {carrousellMovies: productoService.getCarrousell(),movieGridMovies: productoService.getMovieGrid(), topMovies: productoService.getTopMovies()})
 }
 
 
