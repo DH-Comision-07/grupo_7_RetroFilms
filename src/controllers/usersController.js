d75a12e4a18c51519f2d06bbfaa7d1c8fe99f404
VincentVega20 <maximolopardo@gmail.com>
2024-03-31 19:30:42 -0300

    agregado de confirmaciones al poner datos en la vista /registerForm/

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,22 @@
 let products = require('../models/productService.js');
 const path = require('path');
+const { validationResult } = require("express-validator");
 
 const usersController = { 
     login: (req, res) => res.render("users/login"),
-    register: (req, res) => res.render("users/registerForm")
+
+    register: (req, res) => res.render("users/registerForm"),
+    
+    processRegister: (req, res) => {
+        const resultValidation = validationResult(req);
+        
+        if (resultValidation.errors.length > 0) {
+            return res.render("users/registerForm", {
+                errors: resultValidation.mapped(), //array a obj literal
+                oldData: req.body
+            });
+        }
+    }
 }
 
 module.exports = usersController;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = express.Router(); 
+
 const productRouter = require( './product.routes');
 const userRouter = require("./users.routes.js")
+
 const mainController = require("../controllers/mainController.js")
 
 routes.get('/', mainController.index);
