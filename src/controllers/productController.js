f4e80048237fbc581b8c59bd87b5f355dd95c699
Laura <laura@iMac-de-Laura.local>
2024-03-31 16:48:41 -0500

    Fix: header de vistas edition y creation

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,14 @@
-let productService = require('../models/productService.js');
+let productService = require('../models/productService');
 const path = require('path');
 
 
 const productController = { 
-    //productDetail: (req, res) => res.render("products/productDetail/"), //añadí la carpeta products que nos pidieron añadir en el sprint pasado, pues sin ella no renderisa la vista
-    shoppingCart: (req, res) => res.render("products/shoppingCart"), //añadí la carpeta products que nos pidieron añadir en el sprint pasado, pues sin ella no renderisa la vista
-    id: (req,res) => res.render('products/productDetail', {movieDetails: productService.getOneBy(req.params.id)}), // falta el objeto literal para llamar el :id
-    create: (req, res) => res.render('creation'),
-    edit:(req, res) => res.render('edition'),
-    save: (req, res) => res.send('quedó guardado'),
-    add: (req, res) => res.send('quedó guardado') //incorrecto-corregir
-}
+        shoppingCart: (req, res) => res.render("products/shoppingCart"), //añadí la carpeta products que nos pidieron añadir en el sprint pasado, pues sin ella no renderisa la vista
+        id: (req,res) => res.render('products/productDetail', {movieDetails: productService.getOneBy(req.params.id)}), // falta el objeto literal para llamar el :id
+        create: (req, res) => res.render('creation'),
+        edit:(req, res) => res.render('edition'),
+        save: (req, res) => res.send('quedó guardado'),
+        add: (req, res) => res.send(req.body)
+    } 
 
 module.exports = productController;
