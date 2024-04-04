486e9a05d05a1dc8647953e7aed717ba65063815
Laura <laura@iMac-de-Laura.local>
2024-04-04 15:37:20 -0500

    correccion de sintaxis

--- src/models/productService.js
+++ src/models/productService.js
@@ -46,21 +46,21 @@ let productService = {
 
     editMovie:(id,editedMovie) => {
             const moviesFilePath = path.resolve(__dirname, '../models/movies.json'); //Definimos la ruta al json
-            const moviesJSON = JSON.parse(fs.readFileSync(moviesFilePath, 'utf-8')); //leemos el json
+            const moviesINFO = JSON.parse(fs.readFileSync(moviesFilePath, 'utf-8')); //leemos el json
 
-            const movieIndex = moviesJSON.findIndex(movie => movie.id === id); // creamos la variable MovieIndex para encontrar la posicion en el array de peliculas que pertenezca a la pelicula que estamos buscando para editar
+            const movieIndex = moviesINFO.findIndex(movie => movie.id === id); // creamos la variable MovieIndex para encontrar la posicion en el array de peliculas que pertenezca a la pelicula que estamos buscando para editar
 
             if (movieIndex === -1) {
             throw new Error('Película no encontrada'); //condicional donde verificamos si la posición y por ende la pelicula, existe en el array (si arrojara -1, significa que la pelicula no existe y devuelve el error)
             }
 
-            moviesJSON[movieIndex] = {   //Acá accedemos a la posicion en el indes de la pelicula encontrada por id
-                ...moviesJSON[movieIndex],  //hacemos una copia del objeto literal de esa pelicula(id) con spread operator
+            moviesINFO[movieIndex] = {   //Acá accedemos a la posicion en el indes de la pelicula encontrada por id
+                ...moviesINFO[movieIndex],  //hacemos una copia del objeto literal de esa pelicula(id) con spread operator
                 ...editedMovie // Actualiza las propiedades cambiadas o editadas y las lleva a la copia anterior
             };
-            fs.writeFileSync(moviesFilePath, JSON.stringify(moviesJSON)); //escribimos en el json el objeto modificado
+            fs.writeFileSync(moviesFilePath, JSON.stringify(moviesINFO)); //escribimos en el json el objeto modificado
 
-            return moviesJSON[movieIndex] // nos muestra el objeto modificado
+            return moviesINFO[movieIndex] // nos muestra el objeto modificado
     }
 }
 
